8b3c1d219e67d71a034432f1595898c2ad915993
Udeh Ben <udehbudeh@gmail.com>
2021-01-30 23:22:05 +0100

    Feat: db codes for the web app

--- hep_app/app.py
+++ hep_app/app.py
@@ -3,4 +3,70 @@ import streamlit as st, os
 import numpy as np, pandas as pd, matplotlib.pyplot as pt
 import seaborn as sns
 import joblib
+import hashlib
+import matplotlib
+matplotlib.use('Agg')
 
+
+from db_manager import *
+def generate_hashes(password):
+    return hashlib.sha256(str.encode(password)).haxdigest()
+
+def verify_hashes(password,hashed_text):
+    if generate_hashes(password) == hashed_text:
+        return hashed_text
+    return False
+
+
+def main():
+    '''Mortality prediction App'''
+    st.title('Disease Mortality Prediction App')
+
+    menu = ['Home','Login','Signup']
+    submenu = ['Plot','Prediction']
+
+    choice = st.sidebar.selectbox('Menu',menu)
+    if choice == 'Home':
+        st.subheader('Home')
+        st.text('What is Hepatities?')
+
+    elif choice == 'Login':
+        username = st.sidebar.text_input('Username')
+        password = st.sidebar.text_input('Passwprd',type='password')
+        if st.sidebar.checkbox('Login'):
+            if password == '12345':
+                st.success(f'Welcome {username}')
+
+                activity = st.selectbox('Activity',submenu)
+                if activity == 'Plot':
+                    st.subheader('Data Vis Plot')
+
+                elif activity == 'Prediction':
+                    st.subheader('Prediction Analytics')
+            else:
+                st.warning('Incorect Username or Password')
+
+    elif choice == 'Signup':
+        new_username = st.text_input('user name')
+        new_password = st.text_input('Password',type = 'password')
+        confirm_password = st.text_input('Confirm Password',type='password')
+        if new_password == confirm_password:
+            st.success('Password Confirmed')
+        else:
+            st.warning('Password not thesame')
+
+        if st.button('Submit'):
+            create_usertable()
+            hashed_new_password = generate_hashes(new_password)
+            add_userdata(new_username)
+            st.success('You have successfully created a new account')
+            st.info('Login to Get STarted')
+
+
+
+
+
+
+
+if __name__== '__main__':
+    main()
---
+++ hep_app/db_manager.py
@@ -0,0 +1,24 @@
+import sqlite3
+conn = sqlite3.connect('usersdata.db')
+c = conn.cursor()
+
+
+
+def create_usertable():
+    c.execute('CREATE TABLE IF NOT EXISTS userstable(username Text,password TEXT)')
+
+
+def add_userdata(username, password):
+    c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+    conn.commit()
+
+
+def login_user(username,password):
+    c.execute('SELECT * FROM userstable WHERE username=? AND password=?',(username,password))
+    data = c.fetchall()
+    return data
+
+def view_all_users():
+    c.execute('SELECT * FROM userstable')
+    data = c.fetchall()
+    return data
